40085f2828071fb617fc0705a57f4528020b43b4
tmdgus7065 <tmdgus7065>
2020-11-18 19:04:27 +0900

    add_lists

--- anything.py
+++ anything.py
@@ -1,3 +1,5 @@
-# 내용 작성 예시
+from add_lists import add_lists
+
 if __name__ == "__main__":
-    print("hello python, hi ros.")
+    result = add_lists(['wel', 't', 'r'], ['come', 'o', 'os'])
+    print(result)
